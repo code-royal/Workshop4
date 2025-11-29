cb059002332484dfb130e9f359c14c2160cb9bc9
Prajwal Rajbhandari <prajwalrajbhandari8@gmail.com>
2025-11-29 19:53:08 +0545

    commit

--- Divisible_by_two.java
+++ Divisible_by_two.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 /**
  * Write a description of class Divisible_by_two here.
@@ -7,4 +8,18 @@
  */
 public class Divisible_by_two
 {
+    public static void main(String[] Args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter any number: ");
+        int num = input.nextInt();
+        if(num%2==0)
+        {
+            System.out.println(num + " is divisible by 2.");
+        }
+        else 
+        {
+            System.out.println(num + " is not divisible by 2.");
+        }
+    }
 }
